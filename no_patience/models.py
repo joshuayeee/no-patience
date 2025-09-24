5016ede8ac1f8d94320d3599a93c9f968cf163af
joshuayeee <99926652+joshuayeee@users.noreply.github.com>
2025-09-24 11:10:22 -0700

    django setup continued

--- no_patience/models.py
+++ no_patience/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Doctor(models.Model):
+    """Doctor that is generated"""
+    date_added = models.DateTimeField(auto_now_add=True)
+
+
--- np_project/settings.py
+++ np_project/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # My apps.
+    'no_patience',
+    # Default django apps.
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
