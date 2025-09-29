48ea05ee908b5f00a63e6987656c217d667a5b08
joshuayeee <99926652+joshuayeee@users.noreply.github.com>
2025-09-29 11:14:37 -0700

    added more models

--- no_patience/models.py
+++ no_patience/models.py
@@ -5,4 +5,9 @@ class Doctor(models.Model):
     """Doctor that is generated"""
     date_added = models.DateTimeField(auto_now_add=True)
 
+class DoctorChat(models.Model):
+    """Chat about the Doctor"""
+    my_doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
 
+class HomeChat(models.Model):
+    """Chat that will generate a Doctor"""
