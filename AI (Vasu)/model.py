7437fb5ba7815a19fff9373bf06ce5b881dd63bf
Siddharth Vasu <siddharth.vasu.ca@gmail.com>
2026-02-17 11:08:55 -0800

    Switching to Hugging Face model

--- AI (Vasu)/model.py
+++ AI (Vasu)/model.py
@@ -1,3 +1,31 @@
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+import pandas as pd
+
+tokenizer = AutoTokenizer.from_pretrained("vaishali/multitabqa-base")
+model = AutoModelForSeq2SeqLM.from_pretrained("vaishali/multitabqa-base")
+
+# Example tables
+department_df = pd.read_csv("department.csv")
+management_df = pd.read_csv("management.csv")
+
+# Flatten tables into a model-readable string
+def linearize_table(table_name, df):
+    cols = " | ".join(df.columns)
+    rows = " ".join([
+        f"row {i+1} : " + " | ".join(map(str, row)) 
+        for i, row in df.iterrows()
+    ])
+    return f" : {table_name} col : {cols} {rows}"
+
+question = "How many departments are led by heads who are not mentioned?"
+table_context = linearize_table("department", department_df) + linearize_table("management", management_df)
+model_input_string = question + " " + table_context
+
+inputs = tokenizer(model_input_string, return_tensors="pt")
+outputs = model.generate(**inputs)
+print(tokenizer.batch_decode(outputs, skip_special_tokens=True))
+
+'''
 import base64
 import os
 from google import genai
@@ -5,6 +33,7 @@ from google.genai import types
 from google.genai import errors
 import csv
 from pathlib import Path
+import time
 
 # Extracts CSV data
 def extract_csv(pathname: str) -> list[str]:
@@ -43,20 +72,32 @@ def generate():
         )),
     ]
     generate_content_config = types.GenerateContentConfig(
-        thinking_config=types.ThinkingConfig(
-            thinking_level="LOW",
-        ),
         tools=tools,
     )
 
     print("\nResponse:")
-    for chunk in client.models.generate_content_stream(
-        model=model,
-        contents=contents,
-        config=generate_content_config,
-    ):
-        if chunk.text:
-            print(chunk.text, end="")
+    
+    while True:
+        try:
+            for chunk in client.models.generate_content_stream(
+                model=model,
+                contents=contents,
+                config=generate_content_config,
+            ):
+                if chunk.text:
+                    print(chunk.text, end="")
+
+            break
+
+        except errors.ClientError as e:
+            if "429" in str(e):
+                print("\n[Quota reached. Retrying in 30 seconds...]")
+                time.sleep(30)
+            else:
+                raise e
+
 
 if __name__ == "__main__":
     generate()
+
+'''
